d66add0eaef46dc4795e5e910653f7343c17628e
mdhemalakhand1999 <hemalrika@gmail.com>
2024-11-04 19:47:50 +0600

    updated

---
+++ create-react-comp/first-react-comp/src/App.jsx
@@ -0,0 +1,10 @@
+import KgButton from "./KgButton";
+
+function App() {
+  return <>
+    <h1>Hi</h1>
+    <KgButton/>
+  </>
+}
+
+export default App;
---
+++ create-react-comp/first-react-comp/src/KgButton.jsx
@@ -0,0 +1,4 @@
+function KgButton() {
+  return <><button>Like our video</button></>;
+}
+export default KgButton;
