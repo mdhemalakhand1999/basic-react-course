9d371d02da59e701414ebf05669b4e88fcbf537f
mdhemalakhand <hemalrika@gmail.com>
2024-11-25 09:26:27 +0600

    updated

---
+++ projects/7-social-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+
+function App() {
+
+  return (
+    <>
+      <Sidebar />
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
